480b22d113fa473112fa3711d284f2c5a55b081e
IgorT <Igtar65@gmail.com>
2024-02-16 22:36:40 +0600

    Les_1/Welcom

---
+++ Les_1.java
@@ -0,0 +1,14 @@
+import java.time.LocalDateTime;
+
+/**
+ * Les_1
+ */
+public class Les_1 {
+
+       public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Hello World ! Welcom to Java ");
+        System.out.println(LocalDateTime.now());
+        System.out.println();
+    }
+}
