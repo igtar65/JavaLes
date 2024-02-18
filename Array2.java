06f3959b3e16379e8e370d714cf8cc492373059e
IgorT <Igtar65@gmail.com>
2024-02-19 01:37:14 +0600

    Инициализацияи Вывод эл.массива

--- Array2.java
+++ Array2.java
@@ -1,25 +1,20 @@
 /*Одномерные массивы -инициализация и вывод элементов[] */
 
+import java.util.Arrays;
+
 public class Array2 {
    
         public static void main(String[] args) {
-            int[]mAr = {1, 2, 3, 4};
+            int[]mAr = {1, 2, 3, 4};               // инициализация
+            for (int element :mAr) {
+                System.out.print(element + " ");    //вывод эл.
+            }
+            System.out.println();
+           mAr = new int[]{4,1,2,3};                  // или так и сортировка
+           Arrays.sort(mAr, 0, 3);      
             for (int element :mAr) {
                 System.out.print(element + " ");
             }
             System.out.println();
-           mAr = new int[]{4, 7, 2};
-            for (int element :mAr) {
-                System.out.print(element + " ");
-            }
-            System.out.println();
-    
-            print(new int[]{4, 6, 2, 3});
         }
-    
-        public static void print(int[] array) {
-            for (int element : array) {
-                System.out.print(element + " ");
-            }
-        }
-}
+    }
