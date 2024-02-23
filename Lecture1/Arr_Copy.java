ff1c99f8c530eccd8cef6e3f620c58957611ce65
IgorT <Igtar65@gmail.com>
2024-02-23 10:36:08 +0600

    Копирование Массивов

---
+++ Lecture1/Arr_Copy.java
@@ -0,0 +1,26 @@
+package Lecture1;
+
+/*Копирование массива-метод Arrays.copyOf()
+ * целиком Arr1...или часть -Arr2-copyOf(arr1, 4) и Arr3(по заданным индексам)-Arrays.copyOfRange(arr1, 3,6)
+ */ 
+
+ import java.util.Arrays;
+
+ public class Arr_Copy {
+     public static void main(String[] args) {
+ 
+         int[]arr1 = {167, -2, 16, 99, 26, 92, 43, -234, 35, 80};
+  
+         int []arr1Copy = Arrays.copyOf(arr1,arr1.length);
+         System.out.println(Arrays.toString(arr1Copy));
+       
+         int [] arr2Copy = Arrays.copyOf(arr1, 4);            // копируем -от 0 до 4 элемента
+         System.out.println(Arrays.toString(arr2Copy) + " -copyOf");   //выводим
+ 
+          int [] arr3Copy = Arrays.copyOfRange(arr1,3,6);    // от 3 до 6 элемента -по заданным индексам
+         System.out.println(Arrays.toString(arr3Copy) + " -copyOfRange");
+     }
+  }
+    
+ 
+ 
